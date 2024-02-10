3c6509b54238f358c3aadd9fe501ea91bffa0438
ayhonz <janmachacek97@gmail.com>
2024-02-10 17:30:32 +0100

    msg from one client is send to all clients

--- main.go
+++ main.go
@@ -5,19 +5,50 @@ import (
 	"net"
 )
 
-func handleConnection(c net.Conn) {
-	log.Printf("connection: %s", c.RemoteAddr().String())
-	s := "Hello There"
+type MsgType int
 
+const (
+	ClientConnected MsgType = iota
+	NewMsg
+)
+
+type msg struct {
+	Text string
+	Conn net.Conn
+	Type MsgType
+}
+
+func serve(messages <-chan msg) {
+	var connections []net.Conn
+
+	for {
+		msg := <-messages
+		switch msg.Type {
+		case ClientConnected:
+			connections = append(connections, msg.Conn)
+		case NewMsg:
+			for _, conn := range connections {
+				conn.Write([]byte(msg.Text))
+			}
+
+		}
+	}
+
+}
+
+func handleConnection(c net.Conn, messages chan<- msg) {
+	log.Printf("connection: %s", c.RemoteAddr().String())
+	s := "Welcome to Hell!!\n"
 	c.Write([]byte(s))
 
 	for {
-		buff := make([]byte, 513)
-		_, err := c.Read(buff)
+		buf := make([]byte, 513)
+		_, err := c.Read(buf)
 		if err != nil {
 			log.Fatalf("Failed to read from conn: %v", err)
 		}
-		log.Printf("Reading: %s", buff)
+		messages <- msg{Type: NewMsg, Conn: c, Text: string(buf)}
+		log.Printf("Reading: %s", buf)
 	}
 }
 
@@ -26,6 +57,8 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	messages := make(chan msg)
+	go serve(messages)
 
 	for {
 		conn, err := nl.Accept()
@@ -33,7 +66,8 @@ func main() {
 			panic(err)
 		}
 
-		go handleConnection(conn)
+		messages <- msg{Conn: conn, Type: ClientConnected}
+		go handleConnection(conn, messages)
 	}
 
 }
